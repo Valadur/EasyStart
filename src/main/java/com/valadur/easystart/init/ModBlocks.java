31f08695ed6428de5b29797226c96f3747de5c7d
valadur <andreasgrassl@gmx.de>
2014-07-14 16:17:16 +0200

    some more cleanup

--- src/main/java/com/valadur/easystart/init/ModBlocks.java
+++ src/main/java/com/valadur/easystart/init/ModBlocks.java
@@ -3,9 +3,11 @@ package com.valadur.easystart.init;
 import com.valadur.easystart.block.BlockES;
 import com.valadur.easystart.block.AnderiumOre;
 import com.valadur.easystart.reference.Names;
+import com.valadur.easystart.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
 
     public static BlockES oreAnderium = new AnderiumOre();
