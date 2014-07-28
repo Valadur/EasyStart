f4235e2468e252c70f5d0be33f2836329140005e
valadur <andreasgrassl@gmx.de>
2014-07-28 11:47:29 +0200

    added upgrades for vanilla pickaxes, axes, shovels, and swords

--- src/main/java/com/valadur/easystart/init/ModRecipes.java
+++ src/main/java/com/valadur/easystart/init/ModRecipes.java
@@ -1,6 +1,7 @@
 package com.valadur.easystart.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import jdk.nashorn.internal.ir.Block;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -20,11 +21,29 @@ public class ModRecipes {
         GameRegistry.addShapelessRecipe(new ItemStack(Items.cookie,4), new Object[]{Blocks.dirt});
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.anderiumIngotEnergized), new Object[]{ModItems.anderiumIngot, Items.redstone});
 
+        // Upgrades for pickaxes
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_pickaxe), new Object[]{new ItemStack(Items.wooden_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_pickaxe), new Object[]{new ItemStack(Items.stone_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_pickaxe), new Object[]{new ItemStack(Items.iron_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+
+        //Upgrades for axes
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_axe), new Object[]{new ItemStack(Items.wooden_axe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_axe), new Object[]{new ItemStack(Items.stone_axe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_axe), new Object[]{new ItemStack(Items.iron_axe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+
+        // Upgrades for shovels
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_shovel), new Object[]{new ItemStack(Items.wooden_shovel, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_shovel), new Object[]{new ItemStack(Items.stone_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_shovel), new Object[]{new ItemStack(Items.iron_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+
+        // Upgrades for swords
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_sword), new Object[]{new ItemStack(Items.wooden_sword, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_sword), new Object[]{new ItemStack(Items.stone_sword, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_sword), new Object[]{new ItemStack(Items.iron_sword, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+
+
+
         // Shaped recipes
-        //GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumAxe, 1), "xx ", "xy ", " y ", 'x', new ItemStack(ModItems.anderiumIngot), 'y', new ItemStack(Items.stick,OreDictionary.WILDCARD_VALUE));
-        //GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumPickaxe, 1), "xxx", " y ", " y ", 'x', new ItemStack(ModItems.anderiumIngot), 'y', new ItemStack(Items.stick,OreDictionary.WILDCARD_VALUE));
-        //GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumShovel, 1), " x ", " y ", " y ", 'x', new ItemStack(ModItems.anderiumIngot), 'y', new ItemStack(Items.stick,OreDictionary.WILDCARD_VALUE));
-        //GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumSword, 1), " x ", " x ", " y ", 'x', new ItemStack(ModItems.anderiumIngot), 'y', new ItemStack(Items.stick,OreDictionary.WILDCARD_VALUE));
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumHelmet, 1), "xxx", "x x", "   ", 'x', new ItemStack(ModItems.anderiumIngot));
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumHelmet, 1), "   ", "xxx", "x x", 'x', new ItemStack(ModItems.anderiumIngot));
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.anderiumChestplate, 1), "x x", "xxx", "xxx", 'x', new ItemStack(ModItems.anderiumIngot));
@@ -37,7 +56,6 @@ public class ModRecipes {
         GameRegistry.addShapedRecipe(ModItems.enchantedAnderiumLeggings, "xxx", "x x", "x x", 'x', new ItemStack(ModItems.anderiumIngotEnergized));
         GameRegistry.addShapedRecipe(ModItems.enchantedAnderiumBoots, "x x", "x x", "   ", 'x', new ItemStack(ModItems.anderiumIngotEnergized));
         GameRegistry.addShapedRecipe(ModItems.enchantedAnderiumBoots, "   ", "x x", "x x", 'x', new ItemStack(ModItems.anderiumIngotEnergized));
-        //GameRegistry.addShapedRecipe(ModItems.enchantedAnderiumPickaxe, "xxx", " y ", " y ", 'x', new ItemStack(ModItems.anderiumIngotEnergized), 'y', new ItemStack(Items.stick,OreDictionary.WILDCARD_VALUE));
 
         // Oredictionary recipes
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.anderiumSword, 1), " x ", " x ", " y ", 'x', new ItemStack(ModItems.anderiumIngot), 'y', "stickWood"));
