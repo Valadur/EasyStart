9d89e0655cf80e38d54beeda2fbd2ef15b4fc1ae
valadur <andreasgrassl@gmx.de>
2014-07-28 11:53:09 +0200

    cleanup for the shapeless recipes

--- src/main/java/com/valadur/easystart/init/ModRecipes.java
+++ src/main/java/com/valadur/easystart/init/ModRecipes.java
@@ -1,14 +1,12 @@
 package com.valadur.easystart.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import jdk.nashorn.internal.ir.Block;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class ModRecipes {
 
@@ -18,28 +16,28 @@ public class ModRecipes {
         enchantItemStacks();
 
         // Shapeless recipes
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.cookie,4), new Object[]{Blocks.dirt});
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.anderiumIngotEnergized), new Object[]{ModItems.anderiumIngot, Items.redstone});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.cookie,4), Blocks.dirt);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.anderiumIngotEnergized), ModItems.anderiumIngot, Items.redstone);
 
         // Upgrades for pickaxes
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_pickaxe), new Object[]{new ItemStack(Items.wooden_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_pickaxe), new Object[]{new ItemStack(Items.stone_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_pickaxe), new Object[]{new ItemStack(Items.iron_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_pickaxe), new ItemStack(Items.wooden_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_pickaxe), new ItemStack(Items.stone_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_pickaxe), new ItemStack(Items.iron_pickaxe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond);
 
         //Upgrades for axes
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_axe), new Object[]{new ItemStack(Items.wooden_axe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_axe), new Object[]{new ItemStack(Items.stone_axe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_axe), new Object[]{new ItemStack(Items.iron_axe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_axe), new ItemStack(Items.wooden_axe, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_axe), new ItemStack(Items.stone_axe, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_axe), new ItemStack(Items.iron_axe, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond);
 
         // Upgrades for shovels
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_shovel), new Object[]{new ItemStack(Items.wooden_shovel, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_shovel), new Object[]{new ItemStack(Items.stone_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_shovel), new Object[]{new ItemStack(Items.iron_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_shovel), new ItemStack(Items.wooden_shovel, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_shovel), new ItemStack(Items.stone_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_shovel), new ItemStack(Items.iron_shovel, 1, OreDictionary.WILDCARD_VALUE), Items.diamond);
 
         // Upgrades for swords
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_sword), new Object[]{new ItemStack(Items.wooden_sword, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone, Blocks.cobblestone});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_sword), new Object[]{new ItemStack(Items.stone_sword, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot, Items.iron_ingot});
-        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_sword), new Object[]{new ItemStack(Items.iron_sword, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond, Items.diamond});
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.stone_sword), new ItemStack(Items.wooden_sword, 1, OreDictionary.WILDCARD_VALUE), Blocks.cobblestone, Blocks.cobblestone);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.iron_sword), new ItemStack(Items.stone_sword, 1, OreDictionary.WILDCARD_VALUE), Items.iron_ingot, Items.iron_ingot);
+        GameRegistry.addShapelessRecipe(new ItemStack(Items.diamond_sword), new ItemStack(Items.iron_sword, 1, OreDictionary.WILDCARD_VALUE), Items.diamond, Items.diamond);
 
 
 
