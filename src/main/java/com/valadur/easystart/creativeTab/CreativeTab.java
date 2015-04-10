0b68539a9e85088d63747aec453616368c93b3a2
Valadur <if2510@thi.de>
2015-04-11 01:21:02 +0200

    now i can start updating

--- src/main/java/com/valadur/easystart/creativeTab/CreativeTab.java
+++ src/main/java/com/valadur/easystart/creativeTab/CreativeTab.java
@@ -2,11 +2,8 @@ package com.valadur.easystart.creativeTab;
 
 import com.valadur.easystart.init.ModItems;
 import com.valadur.easystart.reference.Reference;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.util.StatCollector;
 
 public class CreativeTab {
     public static final CreativeTabs ES_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase()) {
